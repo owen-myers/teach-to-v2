882b1915d9151330cd4660823a443662bbf9fb73
owen-myers <74884436+owen-myers@users.noreply.github.com>
2024-05-07 15:02:54 -0400

    updated clickable logo

--- src/app/components/teachto-logo.js
+++ src/app/components/teachto-logo.js
@@ -2,11 +2,10 @@ import Link from 'next/link';
 
 export default function TeachToLogo(){
     return (
-        <div className="flex items-center border-2 border-white p-3 rounded-lg">
+        <div className="flex items-center border-2 border-white p-8 rounded-lg">
             <Link href="/">
-                <img src="../TeachTo_Scribble.png" alt="Scribble" className="h-20 w-auto"/>
+                <h2 className="text-2xl font-semibold">TeachTo</h2>
             </Link>
-            <h2 className="text-2xl font-semibold">TeachTo</h2>
         </div>
     );
 };
