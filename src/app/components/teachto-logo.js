07160d667f1eb163276dc440942c3c7603a926d7
owen-myers <74884436+owen-myers@users.noreply.github.com>
2025-02-24 10:30:50 -0500

    added beta and mistake text

--- src/app/components/teachto-logo.js
+++ src/app/components/teachto-logo.js
@@ -5,6 +5,7 @@ export default function TeachToLogo() {
         <div className="flex items-center p-8 rounded-lg">
             <Link href="/">
                 <h2 className="text-2xl font-lora">TeachTo</h2>
+                <p className="font-karla text-sm text-purple-600">(Beta)</p>
             </Link>
         </div>
     );
